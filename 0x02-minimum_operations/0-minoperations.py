246b0515fc8cff2f6420a41423758a8d571037cb
oladakinjoye <oladakinjoye@gmail.com>
2024-07-10 15:45:08 -0700

    update mini operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-""" Minimum Operations"""
+""" Minimum operations"""
 
 
 def minOperations(n):
-    if n <= 0:
+    """
+    miniOperations
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
-
-    # Edge case: If n is a power of 2, we can achieve it with only 2 operations
-    if (n & (n - 1)) == 0:
-        return 2
-
-    # Initialize the number of operations to 0
-    operations = 0
-
-    # Keep dividing n by 2 until it becomes 1
-    while n > 1:
-        # If n is odd, we need an extra paste operation
-        if n % 2 != 0:
-            operations += 1
-            n -= 1
-        # Divide n by 2
-        n //= 2
-        # Increment the number of operations
-        operations += 1
-
-    # We need one more paste operation to get the final result
-    operations += 1
-
-    return operations
+    opns, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            opns += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller values that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return opns
